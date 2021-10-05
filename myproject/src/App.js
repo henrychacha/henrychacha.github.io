a542c32ffa4328b8c954bff41ffedaac50f5a3ad
neon <tongxuhong1997@gmail.com>
2021-10-05 14:18:33 +0800

    me

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './asset/avator.jpg';
 import './App.css';
 
 function App() {
@@ -7,17 +7,6 @@ function App() {
       <div>my name is tam shu hong</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
