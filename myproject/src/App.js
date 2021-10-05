bb835f012ea83f3750a8463a18a97e9e4d875b48
neon <tongxuhong1997@gmail.com>
2021-10-05 15:10:07 +0800

    23

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <div>my name is tam shu hong</div>
+      <div>this is my first object</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
